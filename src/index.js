52a9efd864ebf55567b23436fbdeb0cd70767107
pikokr <parangee9706@gmail.com>
2020-11-21 14:59:02 +0900

    feat: install i18next

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Root from './Root'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <Root/>,
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Layout from '../../components/Layout';
 
 class Home extends Component {
     render() {
         return (
-            <div>
+            <Layout>
                 홈
-            </div>
+            </Layout>
         );
     }
 }
