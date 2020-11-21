d6934149255d5949fe004c559a47686dc37bca23
pikokr <parangee9706@gmail.com>
2020-11-21 15:07:55 +0900

    feat: basic navbar

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     const {t} = useTranslation()
 
     return (
         <>
-            <Navbar>
-                {t('common.title')}
+            <Navbar bg="primary" className="navbar-dark">
+                <Navbar.Brand as={Link} to="/">
+                    {t('common.title')}
+                </Navbar.Brand>
             </Navbar>
         </>
     );
