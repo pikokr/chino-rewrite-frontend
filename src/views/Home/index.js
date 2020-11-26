0871a57acc96e15abe0801728a804c03abe9dfbf
pikokr <parangee9706@gmail.com>
2020-11-26 18:30:53 +0900

    remove cluster list on home

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
-import {withState} from "../../store";
 
 class Home extends Component {
     render() {
         return (
             <Layout>
-                {JSON.stringify(this.props.clusters)}
             </Layout>
         );
     }
 }
 
-export default withState(Home);
+export default Home;
