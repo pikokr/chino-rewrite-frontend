96ad09f92b002747adda1f8c75bdf997e26ed542
pikokr <parangee9706@gmail.com>
2020-11-27 15:03:17 +0900

    feat: commands view

--- src/views/commands/index.js
+++ src/views/commands/index.js
@@ -2,8 +2,31 @@ import React, {Component} from 'react';
 import {withState} from "../../store";
 import Socket from "../../Socket";
 import Layout from "../../components/Layout";
-import {withTranslation} from "react-i18next";
+import {useTranslation, withTranslation} from "react-i18next";
 import classes from './commands.module.css'
+
+const CommandCard = ({r,j}) => {
+    const {t} = useTranslation()
+    const ref = React.useRef()
+    return (
+        <div key={j} className="col">
+            <div className="card">
+                <div className="card-body">
+                    <div style={{
+                        display: 'flex'
+                    }}>
+                        <span style={{flexGrow: 1}} className="p-2">{t(`commands.${r}.name`)}</span>
+                        <button onClick={() => ref.current.classList.toggle('show')} className="btn btn-primary">더보기</button>
+                    </div>
+                    <div className="collapse" ref={ref}>
+                        <hr/>
+                        {t(`commands.${r}.desc`)}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 class CommandsView extends Component {
     componentDidMount() {
@@ -24,9 +47,9 @@ class CommandsView extends Component {
                                     t(`categories.${category.name}`)
                                 }</a>,
                                 <div key={i} className={`tab-pane fade ${i===0?'show active' : ''}`} id={`categories__tab__btn${i}`}>
-                                    {category.commands.map((r, i)=><div key={i}>
-                                        {t(`commands.${r}.name`)}
-                                    </div>)}
+                                    <div className="row row-cols-1 g-4">
+                                        {category.commands.map((r, j)=><CommandCard i={i} r={r} key={j}/>)}
+                                    </div>
                                 </div>
                             ]
                         ))
