3848a8050117e54a3c98d7ab7ab569b086aaafe7
pikokr <parangee9706@gmail.com>
2020-11-29 12:00:40 +0900

    feat: sidebar link

--- src/components/GuildLayout/index.js
+++ src/components/GuildLayout/index.js
@@ -1,6 +1,16 @@
 import React, {Component} from 'react';
 import Layout from "../Layout";
 import styles from './guildLayout.module.css'
+import {Link} from "react-router-dom";
+import {withRouter} from "react-router-dom";
+
+const Item = withRouter(({children, path, location}) => {
+    const active = (location.pathname.split('/').slice(3).join('/') || '/') === path
+    const baseURL = location.pathname.split('/').slice(0,3).join('/')
+    return <li className="nav-item">
+        <Link to={baseURL + path} className={`nav-link ${active ? 'text-dark bg-white' : 'text-white'}`}>{children}</Link>
+    </li>
+})
 
 class GuildLayout extends Component {
     render() {
@@ -10,7 +20,9 @@ class GuildLayout extends Component {
                     <aside className={`${styles.sidebar} bg-dark collapse`} style={{
                         height: '100%'
                     }}>
-                        사이드바
+                        <ul className={`pt-1 flex-column nav list-unstyled`}>
+                            <Item path="/">메인</Item>
+                        </ul>
                     </aside>
                     <div className={`${styles.main} container`}>
                         <h1>TITLE</h1>
