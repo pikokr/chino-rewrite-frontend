ae5aff8a1820767d2c1f977f3c028aceffb278d8
pikokr <parangee9706@gmail.com>
2020-11-29 11:37:44 +0900

    fix: padding

--- src/components/GuildLayout/index.js
+++ src/components/GuildLayout/index.js
@@ -7,12 +7,12 @@ class GuildLayout extends Component {
         return (
             <Layout offset={50}>
                 <div className={styles.layout}>
-                    <aside className={`${styles.sidebar} bg-dark`} style={{
+                    <aside className={`${styles.sidebar} bg-dark collapse`} style={{
                         height: '100%'
                     }}>
                         사이드바
                     </aside>
-                    <div className={styles.main}>
+                    <div className={`${styles.main} container`}>
                         <h1>TITLE</h1>
                         {this.props.children}
                     </div>
