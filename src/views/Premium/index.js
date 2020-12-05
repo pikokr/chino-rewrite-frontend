818e3f4598db6cf45cfc231808f3483ba541e590
pikokr <parangee9706@gmail.com>
2020-12-05 14:16:41 +0900

    feat: premium page

--- src/views/Premium/index.js
+++ src/views/Premium/index.js
@@ -1,10 +1,44 @@
 import React, {Component} from 'react';
 import Layout from "../../components/Layout";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 class Premium extends Component {
     render() {
         return (
             <Layout>
+                <div className="container">
+                    <h2>치노봇 프리미엄</h2>
+                    <small>치노봇 프리미엄에 가입하면 여러 혜택들을 이용하실 수 있어요!</small>
+                    <div className="row g-3 row-cols-md-3 row-cols-1">
+                        <div className="col">
+                            <div className="card">
+                                <div className="card-body text-center">
+                                    <FontAwesomeIcon size="2x" icon={['fas', 'terminal']} style={{marginRight: 10}}/>
+                                    <h4>실험 기능 제공</h4>
+                                    <span>치노봇에 추가 예정인 기능들을 미리 만나보실 수 있어요!</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="card">
+                                <div className="card-body text-center">
+                                    <FontAwesomeIcon size="2x" icon={['fas', 'terminal']} style={{marginRight: 10}}/>
+                                    <h4>실험 기능 제공</h4>
+                                    <span>치노봇에 추가 예정인 기능들을 미리 만나보실 수 있어요!</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="card">
+                                <div className="card-body text-center">
+                                    <FontAwesomeIcon size="2x" icon={['fas', 'terminal']} style={{marginRight: 10}}/>
+                                    <h4>실험 기능 제공</h4>
+                                    <span>치노봇에 추가 예정인 기능들을 미리 만나보실 수 있어요!</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </Layout>
         );
     }
